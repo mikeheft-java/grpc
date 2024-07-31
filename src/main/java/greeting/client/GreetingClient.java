0d87adf79e1e636175bde9c3400b978db2dc2ffc
Mike Heft <mikeheft@gmail.com>
2024-07-31 12:05:31 -0600

    Add sum service

--- src/main/java/greeting/client/GreetingClient.java
+++ src/main/java/greeting/client/GreetingClient.java
@@ -1,18 +1,39 @@
 package greeting.client;
 
+import com.proto.greeting.GreetingRequest;
+import com.proto.greeting.GreetingResponse;
+import com.proto.greeting.GreetingServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
 public class GreetingClient {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Need one argument to work");
+            return;
+        }
+
         ManagedChannel channel = ManagedChannelBuilder
                 .forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
-        // do something
+        switch (args[0]) {
+            case "greet": doGreet(channel); break;
+            default:
+                System.out.println("Keyword Invalid: " + args[0]);
+        }
 
         System.out.println("Shutting down");
         channel.shutdown();
+    }
+
+    private static void doGreet(ManagedChannel channel) {
+        System.out.println("Entered doGreet");
+
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+        GreetingResponse response = stub.greet(GreetingRequest.newBuilder().setFirstName("Mike").build());
+
+        System.out.println("Greeting: " + response.getResult());
     }
 }
